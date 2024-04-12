0559794d419065800f332fdd843cce47204a5e85
mounikas77 <mounikabujji18@gmail.com>
2024-04-12 15:15:33 +0530

    python project-1

---
+++ pythonProject1/main.py
@@ -0,0 +1,28 @@
+# sample_list=[1,2,3,4,5,6,7,8,9,10] #indexing
+# print(sample_list[-2])
+# print(sample_list[4])
+# print(type(sample_list))
+# print(sample_list[0:4])#slicing
+# print(sample_list[-7:])
+# print(sample_list[-7:-2])
+# print(sample_list[:-10])
+# name="i am mounika" #string slicing
+# print(type(name))
+# print(name[0:16])
+# print(name[0])
+# print(name[2:4])
+# print(name[5:10])
+# print(name[4:12])
+# # print(sample_list.)
+# print(list(range(1,101))) #range(start,stop,step)
+# print(list(range(1,21,1)))#here 1 to 20 all numbers display
+# print(list(range(21,2)))#empty list
+# print(list(range(1,21,2)))#odd numbers
+# print(list(range(2,21,2)))#even numbers
+# print(sample_list[1:9:2])
+# print(sample_list[::-1])#revwrse numbers
+# print(sample_list[::-2]) #reverse the numbers and less 2
+# print(name[::-1]) #reverse the string
+# print(name[::-3])
+# print(name[::-2])
+list = [123456789]
